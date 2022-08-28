bb9ab25f0acdd5cb9974db5b2775ea2ba0af5a1f
Olakunle Boye <kunlexzy@gmail.com>
2022-08-28 07:24:51 +0100

    run first test

--- src/components/ui/MainFooter.vue
+++
@@ -1,6 +0,0 @@
-<template>
-	<footer class="main-footer"><div class="float-right d-none d-sm-block"><b>Version</b> 3.0.5
-  </div> <strong>Copyright © 2020 <a href="#">T40</a>.</strong> All rights
-  reserved.
-</footer>
-</template>
